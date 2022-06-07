67972f319f697534d607a518375f457b9e4273f3
vsheviakhova <vsheviakhova@gmail.com>
2022-06-07 14:49:54 +0300

    first draft

---
+++ 2_4.py
@@ -0,0 +1,7 @@
+rect_a = int(input('Сторона прямоугольника а: '))
+rect_b = int(input('Сторона прямоугольника b: '))
+
+if rect_a == rect_b:
+    print('Это квадрат')
+else:
+    print('Это прямоугольник')
